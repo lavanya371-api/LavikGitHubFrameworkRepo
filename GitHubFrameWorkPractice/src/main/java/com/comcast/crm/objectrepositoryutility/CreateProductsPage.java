f3bef27eb25d32ae24c8314e005719e13d9508ff
pratap kurni <pratap kurni@LAPTOP-AOULPL3Q>
2025-02-28 12:03:23 +0530

    fixed edits

--- GitHubFrameWorkPractice/src/main/java/com/comcast/crm/objectrepositoryutility/CreateProductsPage.java
+++ GitHubFrameWorkPractice/src/main/java/com/comcast/crm/objectrepositoryutility/CreateProductsPage.java
@@ -3,6 +3,7 @@ package com.comcast.crm.objectrepositoryutility;
 public class CreateProductsPage {
 	public static void main(String[] args) {
 		System.out.println("cpp");
+	System.out.println("fixed");
 	}
 
 }
